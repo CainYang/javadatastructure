54a1ef2da8d69ccf155cd5f221bc45fb81acfd56
Cain <yangchuan@juxiangfen.com>
2020-04-25 21:44:25 +0800

    update git email

--- src/java/AlgorithmMain.java
+++ src/java/AlgorithmMain.java
@@ -6,8 +6,7 @@ public class AlgorithmMain {
         tree.insert(2);
         tree.insert(3);
         tree.insert(4);
-        tree.insert(5);
-        System.out.println(tree.find(5));
+        System.out.println(tree.find(4));
     }
 
 }
