14ac661e29f6f011346cbad8594eba3a773d8748
CainYang <flybirdyang@outlook.com>
2020-04-25 21:49:55 +0800

    test

--- src/java/AlgorithmMain.java
+++ src/java/AlgorithmMain.java
@@ -5,8 +5,7 @@ public class AlgorithmMain {
         tree.insert(1);
         tree.insert(2);
         tree.insert(3);
-        tree.insert(4);
-        System.out.println(tree.find(4));
+        System.out.println(tree.find(3));
     }
 
 }
